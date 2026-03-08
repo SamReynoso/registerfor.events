04f15d05b3deec11a1e640f8d7bc37d49d48d919
Sam Reynoso <sam@ozprealpha.com>
2026-03-08 12:53:44 -0700

    added user to admin panel

--- project/models/admin.py
+++ project/models/admin.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
 
-from models.models import Event, Profile, Registration, RegistrationItem
+from models.models import Event, Profile, Registration, RegistrationItem, User
  
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'username',
+    )
+
+
 
 @admin.register(Registration)
 class RegistrationAdmin(admin.ModelAdmin):
